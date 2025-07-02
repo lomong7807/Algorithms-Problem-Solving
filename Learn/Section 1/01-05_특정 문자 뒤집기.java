ca680f263463c4e5455cc12abcbe6e9d89f1f2ec
lomong7807 <lomong7807@gmail.com>
2025-07-02 10:18:02 +0900

    Learn 01-05

--- Learn/Section 1/01-05_특정 문자 뒤집기.java
+++ Learn/Section 1/01-05_특정 문자 뒤집기.java
@@ -1,39 +1,45 @@
 import java.io.*;
 import java.util.*;
 /*
- * 문제: 대문자와 소문자가 존재하는 문자열을 받은 후 대문자는 소문자로. 소문자는 대문자로 변환 후 출력
+ * 문제: 문자열을 입력받아 뒤집어서 출력.
+        단, 특수문자는 자기 자리에 그대로 있음.
  * 입력: 문자열
- * 출력: 대소문자가 바뀐 문자열
+ * 출력: 문자열만 뒤집힌 문자열
  * 풀이후기:
- *  - Character.isLoweCase()라는 함수로 소문자인지 확인하고 출력하면 됨
- *  - 어려울거 없었음
+ *  - 문제가 좀 모호했는데, 결국 이것도 내가 이해를 좀 잘 못해서 그런거 같다. 문제를 꼼꼼히 읽자.
+ *  - 처음에는 while문 내부에서 lt와 rt를 증감하는 while문을 또 돌렸는데
+ *    그것보단 그냥 조건문으로 하나의 while문에서 돌게끔 하는게 더 가독성이 좋은거 같다.
+ *  - 그리고 알파벳인지 체크하는 Character.isAlphabetic()함수가 있는지 몰라서 구현했는데, 있더라
  * */
 
 public class Main {
 
-    public static String solution() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringBuilder answer = new StringBuilder();
+    public static String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        char[] arr = s.toCharArray();
+        int lt = 0;
+        int rt = arr.length - 1;
 
-        for(int i = 0; i < n; i++) {
-            char[] arr = br.readLine().toCharArray();
-            int lt = 0, rt = arr.length - 1;
-
-            while(lt < rt) {
+        while(lt < rt) {
+            if(!Character.isAlphabetic(arr[lt])) {
+                lt++;
+            }else if(!Character.isAlphabetic(arr[rt])) {
+                rt--;
+            }else {
                 char tmp = arr[lt];
                 arr[lt] = arr[rt];
                 arr[rt] = tmp;
                 lt++;
                 rt--;
             }
-            answer.append(String.valueOf(arr)).append("\n");
         }
 
-        return answer.toString();
+        return String.valueOf(arr);
     }
-
+    
     public static void main(String[] args) throws IOException {
-        System.out.print(solution());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.print(solution(s));
     }
 }
