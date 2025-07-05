58f79c1345f4a7f6e163d9f7c8dc49e58c3caef7
lomong7807 <lomong7807@gmail.com>
2025-07-05 15:58:57 +0900

    Learn 01-11

---
+++ Learn/Section 1/01-11_문자열 압축.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+/*
+ * 문제: 알파벳 대문자로 이루어진 문자열을 입력받아 같은 문자가 연속으로 반복되는 경우 반복되는 숫자 우측에 숫자 출력
+        단, 반복횟수가 1인 경우 생략
+ * 입력: 알파벳 대문자로 이루어진 문자열 
+ * 출력: 입력받은 문자열을 가공한 문자열
+ * 풀이후기:
+ *  - 처음에 바로 어떻게 해야될지는 생각이 됐는데, 어떻게 효율적으로 해야될지 몰랐음
+ *  - 처음엔 prev라는 변수에 이전 문자열의 값을 저장해서 비교하는 방식으로 구현했음
+ *  - 하지만 현재 방식이 메모리나 시간 성능이 훨씬 좋음
+ *  - 다음 문자열과 비교하여 count 값으로 count해줌
+ * */
+
+public class Main {
+
+    public static String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        s = s + " ";
+        int count = 1;
+
+        for(int i = 0; i < s.length() - 1; i++) {
+            if(s.charAt(i) == s.charAt(i + 1)) {
+                count++;
+            }else {
+                sb.append(s.charAt(i));
+                if(1 < count) {
+                    sb.append(count);
+                }
+                count = 1;
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.print(solution(str));
+    }
+}
