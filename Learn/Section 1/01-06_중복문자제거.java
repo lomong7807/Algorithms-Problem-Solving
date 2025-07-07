cb03f0fbb8f4d6bdc393fa7539d70257a3b74890
lomong7807 <lomong7807@gmail.com>
2025-07-07 11:03:35 +0900

    Learn 01-06 update

--- Learn/Section 1/01-06_중복문자제거.java
+++ Learn/Section 1/01-06_중복문자제거.java
@@ -7,18 +7,20 @@ import java.util.*;
  * 출력: 중복된 문자를 제거한 문자열
  * 풀이후기:
  *  - 처음엔 중복이라는 말을 보고 자료구조 Set을 생각했는데, 순서도 맞춰야돼서 List를 사용함
- *  - 이번 문제는 어려울게 없었음
+ *  - 그런데 List 자료구조도 굳이 안써도 됐다.
+ *  - 간단하게 String.indexOf()를 사용하면 됐다. 
+ *  - indexOf()는 문자열의 제일 앞에서부터 탐색해 가장 앞에 있는 문자 index를 return한다.
+ *  - 따라서 순회하면서 i의 값과 indexOf()의 값이 같다면 해당 문자가 처음으로 나왔다는 뜻이니 sb에 추가하고
+ *  - 아니라면 추가할 필요가 없음
  * */
 
 public class Main {
 
     public static String solution(String s) {
         StringBuilder sb = new StringBuilder();
-        List<Character> list = new ArrayList<>();
 
         for(int i = 0; i < s.length(); i++) {
-            if(!list.contains(s.charAt(i))) {
-                list.add(s.charAt(i));
+            if(i == s.indexOf(s.charAt(i))) {
                 sb.append(s.charAt(i));
             }
         }
