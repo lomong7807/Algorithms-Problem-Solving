b7a7ff2696db918818f45bcd7670acb7eb897966
lomong7807 <lomong7807@gmail.com>
2025-07-07 11:26:10 +0900

    Learn 01-12 update

--- Learn/Section 1/01-12_암호.java
+++ Learn/Section 1/01-12_암호.java
@@ -7,36 +7,29 @@ import java.util.*;
  * 풀이후기:
  *  - 머릿속으로 어떻게 풀면 되겠다라는 생각은 드는데, 바로 구현이 되지 않았다.
  *  - Math.pow의 return 값은 double이라 int로 형변환 해줘야 함.
+ *  - 또 너무 쉬운 방법이 있었음
+ *  - String.substring()함수로 7개씩 잘라서 해당 부분을 replace()함수로 # -> 1, * -> 0 할 수 있음
+ *  - 또 해당 이진수를 바로 int로 변환할때 인자값으로 2를 넣어주면 2진수로 인식해서 변환해준다.
+ *  - 마지막으로 s.substring()함수를 통해 읽은 부분을 자름
+ *  - replace함수와 Integer.parseInt(n, 2)로 매우 간단하게 풀 수 있는 문제였다.
  * */
 
 public class Main {
 
     public static String solution(int n, String s) {
         StringBuilder sb = new StringBuilder();
-        int start = 0;
-        int end = 7;
-        
 
-        while(end <= s.length()) {
-            int sum = 0;    
-
-            while(start < end) {
-                if(s.charAt(start) == '#') {
-                    sum += (int)Math.pow(2, end - start - 1);
-                }
-                start++;
-            }
-
-            sb.append((char)sum);
-
-            sum = 0;
-            end += 7;
+        for(int i = 0; i < n; i++) {
+            String tmp = s.substring(0, 7).replace('#', '1').replace('*', '0');
+            int num = Integer.parseInt(tmp, 2);
+            sb.append((char)num);
+            s = s.substring(7);
         }
 
         return sb.toString();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         String str = br.readLine();
