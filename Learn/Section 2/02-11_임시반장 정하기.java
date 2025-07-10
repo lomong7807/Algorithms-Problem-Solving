e3c0abf9dbaf7f2f1b51032907ddd70effd92d7a
lomong7807 <lomong7807@gmail.com>
2025-07-10 10:36:06 +0900

    Learn 02-11

--- Learn/Section 2/02-11_임시반장 정하기.java
+++ Learn/Section 2/02-11_임시반장 정하기.java
@@ -7,37 +7,29 @@ import java.util.*;
  * 출력: 임시 반장이 될 수 있는 학생 번호. 단, 여러 명이 가능하다면 가장 작은 번호 출력
  * 풀이후기:
  *  - 배열 반복문을 너무 많이 돌리는데 이래도 되는건가 싶음
+ *  - 추가) 역시 더 좋은 방법이 있었음. 기존에는 boolean[][] 배열을 또 만들어서 체크를 했는데 그렇게 하지 않고 그냥 바로 확인을 할 수 있음
+ *  - 한번이라도 겹쳐졌을때 딱 한 번만 추가를 하면 되기 때문에 break; 조건을 빼먹지 말자.
  * */
 
 public class Main {
 
     public static int solution(int n, int[][] arr) {
         int answer = 0;
-        int count = Integer.MIN_VALUE;
-        boolean[][] cntArr = new boolean[n][n];
+        int max = Integer.MIN_VALUE;
 
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < 5; j++) {
-                for(int k = 0; k < n; k++) {
-                    if(i == k) {
-                        continue;
-                    }
-                    if(arr[i][j] == arr[k][j] && !cntArr[i][k]) {
-                        cntArr[i][k] = true;
-                    }
-                }
-            }
-        }
-
-        for(int i = 0; i < n; i++) {
-            int sum = 0;
+            int count = 0;
             for(int j = 0; j < n; j++) {
-                if(cntArr[i][j] && i != j) {
-                    sum++;
+                for(int k = 0; k < 5; k++) {
+                    if(i != j && arr[i][k] == arr[j][k]) {
+                        count++;
+                        break;
+                    }
                 }
             }
-            if(count < sum) {
-                count = sum;
+
+            if(count > max) {
+                max = count;
                 answer = i + 1;
             }
         }
