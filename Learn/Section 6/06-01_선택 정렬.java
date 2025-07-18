464b63b11a83220883ad72f32ac85d27362e0d04
lomong7807 <lomong7807@gmail.com>
2025-07-18 11:42:15 +0900

    Learn 06-01

--- Learn/Section 6/06-01_선택 정렬.java
+++ Learn/Section 6/06-01_선택 정렬.java
@@ -20,21 +20,20 @@ public class Main {
 
         // 배열의 길이보다 1 작게 순회
         for(int i = 0; i < arr.length - 1; i++) {
-            int min = arr[i];      // 가장 작은 값을 담을 변수 선택
             int minIdx = i;        // 가장 작은 값의 위치 기억
 
-            // 정렬되지 않은 영역을 순회하며 지금 min에 있는 값과 비교하기 위해 정렬된 배열의 바로 다음 index부터 비교
+            // 정렬되지 않은 영역을 순회하며 지금 minIdx 위치에 있는 값과 비교하기 위해 정렬된 배열의 바로 다음 index부터 비교
             for(int j = i + 1; j < arr.length; j++) {
-                // 선택되어있는 min 값과 비교해 더 작은 값이면 min, minIdx 저장
-                if(arr[j] < min) {
-                    min = arr[j];       // 해당 값을 저장
+                // 선택되어있는 min 값과 비교해 더 작은 값이면 minIdx 저장
+                if(arr[j] < arr[minIdx]) {
                     minIdx = j;         // 해당 index를 저장
                 }
             }
 
             // 정렬되지 않은 영역의 순회가 끝나면 위치를 변경
-            arr[minIdx] = arr[i];
-            arr[i] = min;
+            int tmp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = tmp;
         }
 
         for(int n : arr) {
