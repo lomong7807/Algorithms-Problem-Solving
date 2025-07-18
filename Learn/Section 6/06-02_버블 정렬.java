f61a039c958c31e4e9251c4e7d2d9565b244a9c5
lomong7807 <lomong7807@gmail.com>
2025-07-18 11:38:10 +0900

    Learn 06-02

--- Learn/Section 6/06-02_버블 정렬.java
+++ Learn/Section 6/06-02_버블 정렬.java
@@ -17,12 +17,12 @@ public class Main {
     public static String solution(int[] arr) {
         StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i < arr.length; i++) {
-            for(int j = 1; j < arr.length - i; j++) {
-                if(arr[j] < arr[j - 1]) {
+        for(int i = 1; i < arr.length; i++) {
+            for(int j = 0; j < arr.length - i; j++) {
+                if(arr[j + 1] < arr[j]) {
                     int tmp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = tmp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
                 }
             }
         }
