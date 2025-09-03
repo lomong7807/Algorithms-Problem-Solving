b2b2fea4f31597a0a739966d66cf5508c3a5d5a1
lomong7807 <lomong7807@gmail.com>
2025-09-03 23:39:36 +0900

    2579

--- Baekjoon/Silver/Silver III/2579_계단 오르기.java
+++ Baekjoon/Silver/Silver III/2579_계단 오르기.java
@@ -13,16 +13,34 @@ import java.util.*;
 
 public class Main {
 
-    
+    static Integer[] dp;
+    static int[] arr;
+
+    public static int dfs(int n) {
+        if(dp[n] == null) {
+            dp[n] = Math.max(dfs(n - 2), dfs(n - 3) + arr[n - 1]) + arr[n];
+        }
+
+        return dp[n];
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        arr = new int[n + 1];
+        dp = new Integer[n + 1];
 
         for(int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        System.out.print();
+
+        dp[0] = arr[0];
+        dp[1] = arr[1];
+
+        if(2 <= n) {
+            dp[2] = arr[1] + arr[2];
+        }
+
+        System.out.print(dfs(n));
     }
 }
