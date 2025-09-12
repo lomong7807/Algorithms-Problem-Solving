71d1b854ccd37be4e95ceac487c2c1565360922e
lomong7807 <lomong7807@gmail.com>
2025-09-12 23:48:43 +0900

    11279

---
+++ Baekjoon/Silver/Silver II/11279_최대 힙.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+/**
+ * 1. 문제정보
+ *  - 제목 : 특정 거리의 도시 찾기
+ *  - 번호 : 18352
+ *  - 링크 : https://www.acmicpc.net/problem/18352
+ * 2. 풀이핵심
+ *  - bfs
+ *  - 최단 거리
+ * 3. 풀이후기
+ *  - 
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        int max = 0;
+
+        Queue<Integer> Q = new ArrayDeque<>();
+
+        for(int i = 0; i < n; i++) {
+            int input = Integer.parseInt(br.readLine());
+            
+            if(input == 0) {
+                if(Q.isEmpty()) {
+                    sb.append(0).append("\n");
+                }else {
+                    while(true) {
+                        int cur = Q.poll();
+
+                        if(cur == max) {
+                            sb.append(cur).append("\n");
+                            break;
+                        }
+
+                        Q.offer(cur);
+                    }
+                }
+            }else {
+                max = Math.max(max, input);
+                Q.offer(input);
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
