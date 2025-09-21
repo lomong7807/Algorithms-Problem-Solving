d36232590dca5933bb96c9f2aef49c97f416b509
lomong7807 <lomong7807@gmail.com>
2025-09-21 12:20:55 +0900

    11286

--- Baekjoon/Silver/Silver I/11286_절댓값 힙.java
+++ Baekjoon/Silver/Silver I/11286_절댓값 힙.java
@@ -10,7 +10,7 @@ import java.util.*;
  * 3. 풀이후기
  *  - Java의 자료구조인 우선순위 큐를 알고 있다면 매우 쉬운 문제다.
  *  - PriorityQueue라는 자료구조가 있어 해당 자료구조는 기본적으로 내림차순 정렬이 돼있다.
- *  - 이전에 풀었던 11279_최대 힙 문제와 똑같다.
+ *  - 여기서 익명 클래스를 정의해서 순서를 원하는대로 만들 수 있다.
  */
 
 public class Main {
@@ -19,16 +19,48 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Integer> Q = new PriorityQueue<Integer>();
+
+/*
+        // 익명 클래스로 일회성 구현
+        PriorityQueue<Integer> Q = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if(Math.abs(o1) > Math.abs(o2)) {
+                    return Math.abs(o1) - Math.abs(o2);
+                }else if(Math.abs(o1) == Math.abs(o2)) {
+                    return o1 - o2;
+                }else {
+                    return -1;
+                }
+            }
+        });
+*/
+        // 명확한 람다 표현식 사용
+        PriorityQueue<Integer> Q = new PriorityQueue<Integer>((o1, o2) -> {
+                if(Math.abs(o1) > Math.abs(o2)) {
+                    return Math.abs(o1) - Math.abs(o2);
+                }else if(Math.abs(o1) == Math.abs(o2)) {
+                    return o1 - o2;
+                }else {
+                    return -1;
+                }
+        });
+/*
+
+        // 람다 표현식 사용
+        PriorityQueue<Integer> Q = new PriorityQueue<>(
+                Comparator.comparingInt((Integer x) -> Math.abs(x))
+                        .thenComparingInt(Integer::intValue)
+        );
+*/
 
         for(int i = 0; i < n; i++) {
-
+            int input = Integer.parseInt(br.readLine());
             if(input == 0) {
                 if(Q.isEmpty()) {
                     sb.append(0).append("\n");
                 }else {
-                    int x = Math.abs(Q.poll());
-                    sb.append().append("\n");
+                    sb.append(Q.poll()).append("\n");
                 }
             }else {
                 Q.offer(input);
