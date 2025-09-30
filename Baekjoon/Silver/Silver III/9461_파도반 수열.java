ce8e2ce35facd1217333ed9e58f171b2afd077fc
lomong7807 <lomong7807@gmail.com>
2025-09-30 23:08:57 +0900

    9461

---
+++ Baekjoon/Silver/Silver III/9461_파도반 수열.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+/**
+ * 1. 문제정보
+ *  - 제목 : 파도반 수열
+ *  - 번호 : 9461
+ *  - 링크 : https://www.acmicpc.net/problem/9461
+ * 2. 풀이핵심
+ *  - Map
+ * 3. 풀이후기
+ *  - 옷 이름이 겹치지 않아서 Map<String, Integer>로 풀면 되는 문제
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+        }
+    }
+}
