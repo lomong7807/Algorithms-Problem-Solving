59118a7e7e3605ed0d0893308427a7971259bd4c
lomong7807 <lomong7807@gmail.com>
2025-10-02 23:26:45 +0900

    9461

--- Baekjoon/Silver/Silver III/9461_파도반 수열.java
+++ Baekjoon/Silver/Silver III/9461_파도반 수열.java
@@ -14,10 +14,13 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
 
         for(int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
         }
+
+
     }
 }
